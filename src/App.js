9e850e2eb37d8d7e569bc49de673f7b939a42d48
Viktor <kushichka@gmail.com>
2021-04-02 20:17:40 +0200

    First layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React from "react";
 import './App.css';
-import Header from "./Header";
-import Tech from "./Tech";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Tech />
+    <div className="app-wrapper">
+      <header>
+          <img className="logo" src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo" />
+      </header>
+        <nav>
+            <div>
+                <a href="#">Profile</a>
+            </div>
+            <div>
+                <a href="#">Message</a>
+            </div>
+            <div>
+                <a href="#">News</a>
+            </div>
+            <div>
+                <a href="#">Music</a>
+            </div>
+            <div>
+                <a href="#"> ettings</a>
+            </div>
+        </nav>
+        <div className="content">
+            <div>
+                <img className="content-img" src="https://fregataero.ru/images/tours/tours/2594.jpg" alt="image" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
